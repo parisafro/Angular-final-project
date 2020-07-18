eda1f0fb6a1c92098e22e8fe3bf106d132c12a35
parisafro <parisafro.pf@gmail.com>
2020-07-18 19:16:43 +0430

    @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule ],
   declarations: [ AppComponent, MainPageComponent ],
   bootstrap:    [ AppComponent ]
 })
