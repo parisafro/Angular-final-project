5c79f9487350c286eb231f748c3b5a8b6c6582b0
parisafro <parisafro.pf@gmail.com>
2020-07-19 02:23:45 +0430

    mad-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+// material components
 import { MainPageComponent } from './main-page/main-page.component';
-
-
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule ],
-  declarations: [ AppComponent, MainPageComponent ],
+  imports:[
+    BrowserModule, 
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule ],
+  declarations: [ 
+    AppComponent, 
+    MainPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
