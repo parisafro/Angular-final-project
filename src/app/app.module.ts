e2606745aef97397722dc2c3242b4c1e1fe5ad59
parisafro <parisafro.pf@gmail.com>
2020-07-18 18:58:39 +0430

    MainPage works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, MainPageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
