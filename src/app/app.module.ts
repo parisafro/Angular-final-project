64e90897303d0bd83ff4c007fd7007341e3e0394
parisafro <parisafro.pf@gmail.com>
2020-07-19 19:40:30 +0430

    MainPage+Introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +9,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
+import { IntroductionComponent } from './introduction/introduction.component';
 @NgModule({
   imports:[
     BrowserModule, 
@@ -15,10 +17,13 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule ],
+    MatButtonModule,
+    RouterModule.forRoot([
+      {path:'',component:MainPageComponent},
+    ]) ],
   declarations: [ 
     AppComponent, 
-    MainPageComponent ],
+    MainPageComponent, IntroductionComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,21 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-// export interface Tile {
-//   color: string;
-//   cols: number;
-//   rows: number;
-// }
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  //   tiles: Tile[] = [
-  //   { cols: 4, rows: 1, color: '#e5e5e5'},
-  //   { cols: 3, rows: 7, color: '#edf2f4'},
-  //   { cols: 1, rows: 7, color: '#e5e5e5'}
-  // ];
-
 
 }
