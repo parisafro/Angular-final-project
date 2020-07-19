0e58d31cca6954add9d30c4ea846de63ab7e79e3
parisafro <parisafro.pf@gmail.com>
2020-07-19 17:28:01 +0430

    add btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   imports:[
     BrowserModule, 
     FormsModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatGridListModule ],
+    MatGridListModule,
+    MatButtonModule ],
   declarations: [ 
     AppComponent, 
     MainPageComponent ],
