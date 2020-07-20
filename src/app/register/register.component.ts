428443942a2c5ed79be2664f46493d6059164999
parisafro <parisafro.pf@gmail.com>
2020-07-20 13:46:12 +0430

    final level

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,26 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-    submit(form){
+  showerr=false;
+   submit(form){
+
     var firstName = form.firstName;
     console.log(firstName);
   
     var lastName = form.lastName;
     console.log(lastName);
+
+    var gender = form.gender;
+    console.log(gender);
   
     var email= form.email;
     console.log(email);
-
-    var username = form.username;
+    
+     var username= form.username;
     console.log(username);
 
     var pass= form.pass;
     console.log(pass);
 
-    var repass= form.repass;
-    console.log(repass);
-
-    
+     var checkpass= form.checkpass;
+    console.log(checkpass);
+    if(pass==checkpass)
+    {
+      this.showerr=false;
+    }
+    else{
+      this.showerr=true;
+    }
   }
 
 }
